39408f31d42e4b6d3c08fbd5f63a37fd6ab18447
ryansallen98 <115708378+ryansallen98@users.noreply.github.com>
2024-01-16 10:43:28 +0000

    Update .env

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
+const fs = require('fs');
 
 const apiRoutes = require('./routes/apiRoutes');
 
@@ -10,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(express.static(path.join(__dirname, 'build')));
 app.use(express.json());
-app.use(cors()); 
+app.use(cors());
 
 // API Routes
 app.use('/api', apiRoutes);
@@ -20,7 +21,24 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// Check if SSL configuration is provided
+const sslPrivKeyPath = process.env.SSL_PRIVATE_KEY_PATH;
+const sslFullChainPath = process.env.SSL_FULL_CHAIN_PATH;
+
+if (sslPrivKeyPath && sslFullChainPath) {
+  // Use HTTPS if SSL configuration is provided
+  const httpsOptions = {
+    key: fs.readFileSync(sslPrivKeyPath),
+    cert: fs.readFileSync(sslFullChainPath),
+  };
+
+  // Start the server with HTTPS
+  require('https').createServer(httpsOptions, app).listen(PORT, () => {
+    console.log(`Server is running on port ${PORT} with HTTPS`);
+  });
+} else {
+  // Start the server with HTTP
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT} with HTTP`);
+  });
+}
